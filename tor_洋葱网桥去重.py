60b7c6a56ff42d862d32d23c8d570333d91f12ca
wangyifan349 <86041125+wangyifan349@users.noreply.github.com>
2023-04-19 07:52:15 +0000

    Create tor_洋葱网桥去重.py

---
+++ tor_洋葱网桥去重.py
@@ -0,0 +1,16 @@
+import re
+
+bridges = [] # 初始化一个列表来存储洋葱网桥
+with open("bridges.txt", "r") as file: # 打开文件
+    for line in file: # 遍历每一行
+        match = re.search(r"obfs4.*", line) # 使用正则表达式找到所有以"obfs4"开头的行，obfs4 协议中洋葱网桥的长度为 44 个字符
+        if match: # 如果匹配成功
+            bridges.append(match.group()) # 将匹配到的内容添加到列表中
+
+unique_bridges = list(set(bridges)) # 使用set()函数去重，然后转换为列表
+
+with open("bridges.txt", "w") as file: # 打开文件以覆盖写入模式
+    file.write("\n".join(unique_bridges)) # 使用"\n"将列表中的元素连接成字符串，然后写回到文件中
+
+print("总共找到洋葱网桥数量:", len(bridges)) # 输出找到的洋葱网桥总数
+print("去重后洋葱网桥数量:", len(unique_bridges)) # 输出去重后的洋葱网桥数量
