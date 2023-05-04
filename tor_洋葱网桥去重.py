a105d21c20bfea45a962e9ced2eb2bbb3f4210a1
wangyifan349 <86041125+wangyifan349@users.noreply.github.com>
2023-05-04 02:35:18 +0000

    Update tor_洋葱网桥去重.py

--- tor_洋葱网桥去重.py
+++ tor_洋葱网桥去重.py
@@ -1,15 +1,15 @@
 import re
 import socket
 
-def extract_port(bridge):#提取端口号，端口号的前面有冒号，后面是数字。
-    match = re.search(r':(\d+)', bridge)
+def extract_port(bridge):
+    match = re.search(r':(\d+)', bridge)#提取端口号的规则
     if match:
         return int(match.group(1))
     else:
         return None
 
 def extract_ip(bridge):
-    match = re.search(r'(\d+\.\d+\.\d+\.\d+)', bridge)#提取ip4的规则
+    match = re.search(r'(\d+\.\d+\.\d+\.\d+)', bridge)#提取ip4地址的规则
     if match:
         return match.group(1)
     else:
@@ -17,13 +17,11 @@ def extract_ip(bridge):
 
 def test_bridge(bridge_ip, bridge_port):
     try:
-        # 创建socket对象
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # 设置连接超时时间
         sock.settimeout(5)
         # 连接网桥
         sock.connect((bridge_ip, bridge_port))
-        # 关闭socket连接
         sock.close()
         # 返回True，表示网桥可用
         return True
@@ -54,11 +52,11 @@ for bridge in unique_bridges:
 
 # 按端口号排序
 sorted_available_bridges = sorted(available_bridges, key=extract_port)
-# 将端口号为443或80的桥移到列表前面，这里是可用的网桥
+# 将端口号为443或80的桥移到列表前面
 sorted_available_bridges = [bridge for bridge in sorted_available_bridges if ':443' in bridge or ':80' in bridge] + \
                            [bridge for bridge in sorted_available_bridges if ':443' not in bridge and ':80' not in bridge]
 
-sorted_bridges = sorted_available_bridges + ['-----------------------------------------------------'] + unavailable_bridges#上面的网桥可以直连。
+sorted_bridges = sorted_available_bridges + ['--------------'] + unavailable_bridges
 
 with open("bridges.txt", "w") as file: # 打开文件以覆盖写入模式
     file.write("\n".join(sorted_bridges)) # 使用"\n"将列表中的元素连接成字符串，然后写回到文件中
