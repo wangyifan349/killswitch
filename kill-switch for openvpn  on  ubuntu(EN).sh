8a43389a470edc941b7b55b5ebbede55233c30d1
wangyifan349 <86041125+wangyifan349@users.noreply.github.com>
2023-07-25 02:31:16 +0000

    Update kill-switch for openvpn  on  ubuntu(EN).sh

--- kill-switch for openvpn  on  ubuntu(EN).sh
+++ kill-switch for openvpn  on  ubuntu(EN).sh
@@ -1,30 +1,51 @@
 #!/bin/bash
-#This is a script for setting up a kill-switch on Ubuntu.
-#It enables the UFW firewall and blocks all incoming and outgoing connections except for OpenVPN's UDP 1194 port and DNS-over-TLS's TCP 853 port.
-#It also sets up encrypted DNS using CloudFlare's DNS service to ensure no DNS pollution.
-#Finally, it restarts the systemd-resolved service to immediately apply the encrypted DNS settings.
-ufw reset
-#Reset the UFW firewall rules
-#Enable UFW firewall
-sudo ufw enable
-ufw allow ssh
+
+
+
+#这个脚本没有写完请不要用
+
+
+
+
+
+
+
+
+
+
+#
+echo "这是一个用于使用在Ubuntu下，利用xray翻墙后，实现kill-switch的脚本和方法，参考了很多博客和教程编写，但仍然可能有漏洞和疏忽的地方"
+sudo -v
+if [ $? -ne 0 ]; then
+    echo "无法获取sudo权限，法继续执行。"
+    exit 1
+fi
+
+sudo ufw reset
+sudo ufw disable 
 # Set the default UFW rules to deny incoming and outgoing connections
+sudo ufw default deny
 sudo ufw default deny incoming
 sudo ufw default deny outgoing
 ufw allow out on tun0 from any to any
-ufw allow in on tun0 from any to any
+ufw allow in on tun0 from any to any#这是如果你连接了Openvpn时，这个是虚拟网卡，允许虚拟网卡内部任意传输流量
 # Allow OpenVPN UDP 1194 port through the firewall
 sudo ufw allow 1194/udp
 # allow tls over dns port 853
-sudo ufw allow 853
-#Configure DNS-over-TLS to use CloudFlare DNS and prevent DNS poisoning
+#sudo ufw allow 853
+ufw allow from 1.1.1.1 to any port 853 proto tcp
+#allow  dns  over tls
+ufw allow from 172.67.166.125 to any
+ufw allow from 172.67.166.125 to any#放行你的VPS ip,这是cloudflare的ip地址，这里举个例子而以
+echo "设置加密的DNS查询，避免DNS污染"
+
 sudo echo -e "[Resolve]\nDNS=1.1.1.1\nDNSOverTLS=yes">/etc/systemd/resolved.conf
-#The following three lines may cause errors, but one of them should work
+
 systemctl restart systemd-resolved.service
 systemctl restart systemd-resolved
-service systemd-resolved restart
+
 
 # Block all other incoming and outgoing connections
-sudo ufw deny in from any to any
-sudo ufw deny out from any to any
+#sudo ufw deny in from any to any
+#sudo ufw deny out from any to any
 echo "Kill-switch enabled. Only OpenVPN UDP 1194 and DNS-over-TLS TCP 853 ports are allowed through the firewall."
